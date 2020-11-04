9d5c81d510d11a0d343d8f05896358492afc72fd
JOYDEEP DAS <joydeep.das43@gmail.com>
2020-11-04 23:45:12 +0530

    Added AppModule, Extensions and Feature Manager

---
+++ app/src/main/java/com/meshdesh/trifler/common/di/AppModule.kt
@@ -0,0 +1,22 @@
+package com.meshdesh.trifler.common.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideGsonBuilder(): Gson {
+        return GsonBuilder()
+            .excludeFieldsWithoutExposeAnnotation()
+            .create()
+    }
+}
