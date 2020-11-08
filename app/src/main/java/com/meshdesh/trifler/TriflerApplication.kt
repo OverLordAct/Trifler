f0a9cd154bf4546ead9b3db8da176f28dc951a37
JOYDEEP DAS <joydeep.das43@gmail.com>
2020-11-09 00:43:30 +0530

    Added animation to Onboarding

--- app/src/main/java/com/meshdesh/trifler/TriflerApplication.kt
+++ app/src/main/java/com/meshdesh/trifler/TriflerApplication.kt
@@ -1,7 +1,14 @@
 package com.meshdesh.trifler
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TriflerApplication : Application()
+class TriflerApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
--- app/src/main/java/com/meshdesh/trifler/onboarding/view/OnboardingActivity.kt
+++ app/src/main/java/com/meshdesh/trifler/onboarding/view/OnboardingActivity.kt
@@ -1,8 +1,12 @@
 package com.meshdesh.trifler.onboarding.view
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.marginBottom
 import androidx.viewpager2.widget.ViewPager2
 import com.meshdesh.trifler.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -37,13 +41,22 @@ class ViewPagerActivity : AppCompatActivity() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
 
-                if (viewpager.currentItem == 2) {
+                if (viewpager.currentItem == PAGES - 1) {
                     skipButton.visibility = View.INVISIBLE
+                    appearFromBottom(getStarted)
+                    disappearIntoBottom(dots)
                 } else {
-                    skipButton.visibility = View.VISIBLE
+                    setSkipVisible()
+                    disappearIntoBottom(getStarted)
+                    appearFromBottom(dots)
                 }
             }
         })
+    }
+
+    private fun setSkipVisible() {
+        if (skipButton.visibility == View.VISIBLE) return
+        skipButton.visibility = View.VISIBLE
     }
 
     override fun onBackPressed() {
@@ -53,4 +66,49 @@ class ViewPagerActivity : AppCompatActivity() {
             viewpager.currentItem -= 1
         }
     }
+
+    private fun disappearIntoBottom(view: View) {
+        if (view.visibility == View.GONE) return
+
+        val y = (view.getHeightHelper() + view.marginBottom)
+
+        ObjectAnimator.ofFloat(view, View.TRANSLATION_Y, 0F, y).apply {
+            addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationStart(animation: Animator?) {
+                    super.onAnimationStart(animation)
+                    view.visibility = View.VISIBLE
+                }
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    super.onAnimationEnd(animation)
+                    view.visibility = View.GONE
+                }
+            })
+            duration = 300
+            start()
+        }
+    }
+
+    private fun appearFromBottom(view: View) {
+        if (view.visibility == View.VISIBLE) return
+
+        val y = (view.getHeightHelper() + view.marginBottom)
+
+        ObjectAnimator.ofFloat(view, View.TRANSLATION_Y, y, 0F).apply {
+            addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationStart(animation: Animator?) {
+                    super.onAnimationStart(animation)
+                    view.visibility = View.VISIBLE
+                }
+            })
+            duration = 300
+            start()
+        }
+    }
+}
+
+fun View.getHeightHelper(): Float {
+    measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+    val height = measuredHeight
+    return height.toFloat()
 }
