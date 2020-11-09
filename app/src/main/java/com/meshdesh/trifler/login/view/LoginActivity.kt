1abe82be8e256390cc0ac09b9507aebacf7990d6
JOYDEEP DAS <joydeep.das43@gmail.com>
2020-11-09 21:19:01 +0530

    Added Custom Header for Login/Sign up

---
+++ app/src/main/java/com/meshdesh/trifler/login/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.meshdesh.trifler.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.meshdesh.trifler.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
