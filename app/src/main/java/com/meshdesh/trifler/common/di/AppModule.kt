0df0eeba3f8299e8f858e9c9f8d140b631dc1cc2
JOYDEEP DAS <joydeep.das@olx.com>
2020-12-06 03:25:07 +0530

    Added authentication for requests

--- app/src/main/java/com/meshdesh/trifler/common/di/AppModule.kt
+++ app/src/main/java/com/meshdesh/trifler/common/di/AppModule.kt
@@ -2,13 +2,17 @@ package com.meshdesh.trifler.common.di
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.meshdesh.trifler.common.data.TokenAuthenticator
+import com.meshdesh.trifler.common.data.TriflerAPI
 import com.meshdesh.trifler.common.data.calladapters.coroutinecalladapter.CoroutinesCallAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -16,6 +20,8 @@ import javax.inject.Singleton
 class AppModule {
 
     private val baseUrl = "https://trifler.herokuapp.com"
+
+    // TODO
 
     @Singleton
     @Provides
@@ -27,11 +33,24 @@ class AppModule {
 
     @Singleton
     @Provides
-    // TODO Add baseUrl
-    fun providesRetrofitBuilder(): Retrofit.Builder {
+    fun providesRetrofitBuilder(okHttpClient: OkHttpClient): Retrofit.Builder {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
             .addCallAdapterFactory(CoroutinesCallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+    }
+
+    @Singleton
+    @Provides
+    fun providesTriflerAPI(retrofit: Retrofit.Builder): TriflerAPI {
+        return retrofit.build().create(TriflerAPI::class.java)
+    }
+
+    @Provides
+    fun providesHttpClient(tokenAuthenticator: TokenAuthenticator): OkHttpClient {
+        return OkHttpClient.Builder()
+            .authenticator(tokenAuthenticator)
+            .build()
     }
 }
