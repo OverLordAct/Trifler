b259c149f6867fa0c478fdab05a0d008c881fb26
JOYDEEP DAS <joydeep.das@olx.com>
2020-12-29 21:09:28 +0530

    Added Dashboard Activity

---
+++ app/src/main/java/com/meshdesh/trifler/dashboard/view/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.meshdesh.trifler.dashboard.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.meshdesh.trifler.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
