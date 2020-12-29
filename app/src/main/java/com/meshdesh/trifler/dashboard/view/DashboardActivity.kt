5f652da991f2b7e861eb242aeb40ce7acd699492
JOYDEEP DAS <joydeep.das@olx.com>
2020-12-29 23:58:02 +0530

    Created Onboarding Dashboard Fragment

--- app/src/main/java/com/meshdesh/trifler/dashboard/view/DashboardActivity.kt
+++ app/src/main/java/com/meshdesh/trifler/dashboard/view/DashboardActivity.kt
@@ -3,7 +3,9 @@ package com.meshdesh.trifler.dashboard.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.meshdesh.trifler.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
