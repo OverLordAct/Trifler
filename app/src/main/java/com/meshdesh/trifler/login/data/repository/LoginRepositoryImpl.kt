063d946bab6948dcc6ac1f028d02df9eb907c5c6
JOYDEEP DAS <joydeep.das43@gmail.com>
2021-08-07 14:35:32 +0530

    Refactored GenericErrorResponse

--- app/src/main/java/com/meshdesh/trifler/login/data/repository/LoginRepositoryImpl.kt
+++ app/src/main/java/com/meshdesh/trifler/login/data/repository/LoginRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.meshdesh.trifler.login.data.repository
 
 import com.meshdesh.trifler.common.data.api.TriflerAPI
+import com.meshdesh.trifler.common.data.entity.GenericErrorResponse
 import com.meshdesh.trifler.common.data.entity.Result
 import com.meshdesh.trifler.login.data.entity.LoginRequest
 import com.meshdesh.trifler.login.data.entity.LoginResponse
@@ -9,7 +10,7 @@ import javax.inject.Inject
 class LoginRepositoryImpl @Inject constructor(
     private val client: TriflerAPI.UnauthenticatedAPI
 ) : LoginRepository {
-    override suspend fun login(loginRequest: LoginRequest): Result<LoginResponse.Success, LoginResponse.Failure> {
+    override suspend fun login(loginRequest: LoginRequest): Result<LoginResponse.Success, GenericErrorResponse> {
         return client.login(loginRequest)
     }
 }
