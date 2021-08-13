908d414dea5ec5dbab5bb764da3e2d6597e09b9f
JOYDEEP DAS <joydeep.das43@gmail.com>
2021-08-13 22:55:14 +0530

    Refactored CategoryRepository

--- app/src/main/java/com/meshdesh/trifler/category/data/repository/CategoryRepositoryImpl.kt
+++ app/src/main/java/com/meshdesh/trifler/category/data/repository/CategoryRepositoryImpl.kt
@@ -1,16 +1,16 @@
 package com.meshdesh.trifler.category.data.repository
 
-import com.meshdesh.trifler.category.data.entity.AddCategoryRequestBody
+import com.meshdesh.trifler.category.data.entity.AddCategoryRequest
+import com.meshdesh.trifler.category.data.entity.AddCategoryResponse
 import com.meshdesh.trifler.common.data.api.TriflerAPI
 import com.meshdesh.trifler.common.data.entity.GenericErrorResponse
-import com.meshdesh.trifler.common.data.entity.GenericSuccessResponse
 import com.meshdesh.trifler.common.data.entity.Result
 import javax.inject.Inject
 
 class CategoryRepositoryImpl @Inject constructor(
     private val api: TriflerAPI
 ) : CategoryRepository {
-    override suspend fun addCategory(addCategoryRequestBody: AddCategoryRequestBody): Result<GenericSuccessResponse, GenericErrorResponse> {
-        return api.addCategory(addCategoryRequestBody)
+    override suspend fun addCategory(addCategoryRequest: AddCategoryRequest): Result<AddCategoryResponse, GenericErrorResponse> {
+        return api.addCategory(addCategoryRequest)
     }
 }
